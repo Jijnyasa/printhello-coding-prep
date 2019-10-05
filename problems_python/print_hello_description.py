02516fc2f73f0ccdfc741bf9834edf2fabf0216d
sanchjain <sanchjain101@gmail.com>
2019-10-05 18:15:51 +0530

    Printed tuple properly

--- problems_python/print_hello_description.py
+++ problems_python/print_hello_description.py
@@ -7,7 +7,7 @@ Print Hello is a initiative which is led by two coding enthusiasts Naveen and Vi
 def get_print_hello_description():
 
     print_hello_description=["Printhello", "is the best educational gateway for every year to kickstart their coding life. Thapar Alumni who gave their heart in teaching languages and prepare students for their placements now starting the best languages batches C/C++ and Python from 8th October onwards . It not only focus on just completing the course but logic development and making awesome projects with that language is their key goal"]
-    print_hello_tuple = ()
-    for word in print_hello_description:
-        print_ hello_tuple.append(word)
+    print_hello_tuple = tuple(print_hello_description)
     return print_hello_tuple
+
+print(get_print_hello_description())
