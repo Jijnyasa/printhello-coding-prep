21226123d3ac32b0449f86f72c2d34fab7bdada9
RahulGoyalgit <rahulgoyal233.rg@gmail.com>
2019-10-06 21:02:43 +0530

    updated

--- problems_c_c++/ques_1.cpp
+++ problems_c_c++/ques_1.cpp
@@ -11,8 +11,8 @@ char* get_company_name(){
 int main() {
     char *name = get_company_name();
     printf("%s is the best educational gateway for every year to kickstart their coding life. Thapar Alumni "
-    "who gave their heart in teaching languages and prepare students for their placements now starting the best languages batches C/C++ and Python from 8th October onwards."
-    "\n\n%%%% not only focus on just completing the course but logic development and making awesome projects with that language is their key goal", name, name);
-    printf("\n\nRegister now on\nwww.printhello.in/courses");
+           "who gave their heart in teaching languages and prepare students for their placements now starting the best languages batches C/C++ and Python from 8th October onwards."
+   " \n\n%s not only focus on just completing the course but logic development and making awesome projects with that language is their key goal", name, name);
+    printf("\n\nRegister now on\n www.printhello.in/courses");
     return 0;
 }
